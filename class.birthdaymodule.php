1941eada608c51a2651f953f36cdfe357ab57985
bleistivt <nmh.acww@yahoo.de>
2014-09-26 08:17:22 +0200

    restructure plugin, module fetches data

--- class.birthdaymodule.php
+++ class.birthdaymodule.php
@@ -1,19 +1,31 @@
 <?php if (!defined('APPLICATION')) exit();
 class BirthdayModule extends Gdn_Module {
 
-	protected $Birthdays;
-
-	public function __construct($Sender) {
-		parent::__construct($Sender);
-		$this->Birthdays = $Sender->Data('Birthdays', array());
-	}
-
 	public function AssetTarget() {
 		return 'Panel';
 	}
+	
+	protected function GetBirthdays() {
+		$Birthdays = json_decode(Gdn::Get('BirthdayModule.Birthdays'));
+		if ($Birthdays && $Birthdays[0] == date('y-m-d'))
+			return $Birthdays[1];
+		$Px = Gdn::Database()->DatabasePrefix;
+		$Birthdays = Gdn::SQL()
+			->Select('UserID')
+			->From('User')
+			->Where("DATE_FORMAT(DateOfBirth, '%m-%d')", "'".date('m-d')."'", false, false)
+			->Get()
+			->Result(DATASET_TYPE_ARRAY);
+		$Birthdays = ConsolidateArrayValuesByKey($Birthdays, 'UserID');
+		$Birthdays = array_diff($Birthdays, array(Gdn::UserModel()->GetSystemUserID()));
+		Gdn::Set('BirthdayModule.Birthdays', json_encode(array(date('y-m-d'), $Birthdays)));
+		return $Birthdays;
+	}
 
 	public function ToString() {
-		$Birthdays = $this->Birthdays;
+		$Birthdays = $this->GetBirthdays();
+		if (empty($Birthdays))
+			return;
 		Gdn::UserModel()->GetIDs($Birthdays);
 		$Return = '<div class="Box BirthdayModule"><h4>'.Plural(count($Birthdays), T("Today's Birthday"), T("Today's Birthdays")).'</h4><p>';
 		foreach ($Birthdays as $Birthday)
--- class.birthdaymodule.plugin.php
+++ class.birthdaymodule.plugin.php
@@ -11,26 +11,10 @@ $PluginInfo['BirthdayModule'] = array(
 class BirthdayModulePlugin extends Gdn_Plugin {
 
 	public function Base_Render_Before($Sender) {
-		$Birthdays = $this->GetBirthdays();
-		if (empty($Birthdays) || $Sender->MasterView == 'admin')
+		if ($Sender->MasterView == 'admin')
 			return;
-		$Sender->SetData('Birthdays', $Birthdays);
-		$BirthdayModule = new BirthdayModule($Sender);
+		$BirthdayModule = new BirthdayModule();
 		$Sender->AddModule($BirthdayModule);
-	}
-
-	protected function GetBirthdays() {
-		$Birthdays = json_decode(Gdn::Get('BirthdayModule.Birthdays'));
-		if ($Birthdays && $Birthdays[0] == date('y-m-d'))
-			return $Birthdays[1];
-		$Px = Gdn::Database()->DatabasePrefix;
-		$Birthdays = Gdn::SQL()
-			->Query("SELECT UserID FROM {$Px}User WHERE DATE_FORMAT(DateOfBirth, '%m-%d') = '".date('m-d')."'")
-			->Result(DATASET_TYPE_ARRAY);
-		$Birthdays = ConsolidateArrayValuesByKey($Birthdays, 'UserID');
-		$Birthdays = array_diff($Birthdays, array(Gdn::UserModel()->GetSystemUserID()));
-		Gdn::Set('BirthdayModule.Birthdays', json_encode(array(date('y-m-d'), $Birthdays)));
-		return $Birthdays;
 	}
 
 	public function OnDisable() {
