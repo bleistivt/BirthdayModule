b5a2b0b041d59e7f06c0ba5eb4ed8b09fce6867c
bleistivt <nmh.acww@yahoo.de>
2014-09-26 08:32:43 +0200

    vanilla code standards

--- class.birthdaymodule.php
+++ class.birthdaymodule.php
@@ -1,36 +1,42 @@
 <?php if (!defined('APPLICATION')) exit();
+
 class BirthdayModule extends Gdn_Module {
 
-	public function AssetTarget() {
-		return 'Panel';
-	}
-	
-	protected function GetBirthdays() {
-		$Birthdays = json_decode(Gdn::Get('BirthdayModule.Birthdays'));
-		if ($Birthdays && $Birthdays[0] == date('y-m-d/H'))
-			return $Birthdays[1];
-		$Px = Gdn::Database()->DatabasePrefix;
-		$Birthdays = Gdn::SQL()
-			->Select('UserID')
-			->From('User')
-			->Where("DATE_FORMAT(DateOfBirth, '%m-%d')", "'".date('m-d')."'", false, false)
-			->Get()
-			->Result(DATASET_TYPE_ARRAY);
-		$Birthdays = ConsolidateArrayValuesByKey($Birthdays, 'UserID');
-		Gdn::Set('BirthdayModule.Birthdays', json_encode(array(date('y-m-d/H'), $Birthdays)));
-		return $Birthdays;
-	}
+    public function AssetTarget() {
+        return 'Panel';
+    }
 
-	public function ToString() {
-		$Birthdays = $this->GetBirthdays();
-		if (empty($Birthdays))
-			return;
-		Gdn::UserModel()->GetIDs($Birthdays);
-		$Return = '<div class="Box BirthdayModule"><h4>'.Plural(count($Birthdays), T("Today's Birthday"), T("Today's Birthdays")).'</h4><p>';
-		foreach ($Birthdays as $Birthday)
-			$Return .= UserPhoto(Gdn::UserModel()->GetID($Birthday), 'Medium').' ';
-		$Return .= '</p></div>';
-		return $Return;
-	}
+    protected function GetBirthdays() {
+        $Birthdays = json_decode(Gdn::Get('BirthdayModule.Birthdays'));
+        if ($Birthdays && $Birthdays[0] == date('y-m-d/H')) {
+            return $Birthdays[1];
+        }
+        $Px = Gdn::Database()->DatabasePrefix;
+        $Birthdays = Gdn::SQL()
+            ->Select('UserID')
+            ->From('User')
+            ->Where("DATE_FORMAT(DateOfBirth, '%m-%d')", date("'m-d'"), false, false)
+            ->Get()
+            ->Result(DATASET_TYPE_ARRAY);
+        $Birthdays = ConsolidateArrayValuesByKey($Birthdays, 'UserID');
+        Gdn::Set('BirthdayModule.Birthdays', json_encode(array(date('y-m-d/H'), $Birthdays)));
+        return $Birthdays;
+    }
+
+    public function ToString() {
+        $Birthdays = $this->GetBirthdays();
+        if (empty($Birthdays)) {
+            return;
+        }
+        Gdn::UserModel()->GetIDs($Birthdays);
+        $Return = '<div class="Box BirthdayModule"><h4>'
+            .Plural(count($Birthdays), T("Today's Birthday"), T("Today's Birthdays"))
+            .'</h4><p>';
+        foreach ($Birthdays as $Birthday) {
+            $Return .= UserPhoto(Gdn::UserModel()->GetID($Birthday), 'Medium').' ';
+        }
+        $Return .= '</p></div>';
+        return $Return;
+    }
 
 }
--- class.birthdaymodule.plugin.php
+++ class.birthdaymodule.plugin.php
@@ -1,24 +1,25 @@
 <?php if (!defined('APPLICATION')) exit();
 
 $PluginInfo['BirthdayModule'] = array(
-	'Name' => 'Birthday Module',
-	'Description' => 'Adds a module showing today\'s birthdays to the Panel. Requires ProfileExtender with the "Birthday" field enabled.',
-	'Version' => '0.1',
-	'Author' => 'Bleistivt',
-	'AuthorUrl' => 'http://bleistivt.net'
+    'Name' => 'Birthday Module',
+    'Description' => 'Adds a module showing today\'s birthdays to the Panel. Requires ProfileExtender with the "Birthday" field enabled.',
+    'Version' => '0.1',
+    'Author' => 'Bleistivt',
+    'AuthorUrl' => 'http://bleistivt.net'
 );
 
 class BirthdayModulePlugin extends Gdn_Plugin {
 
-	public function Base_Render_Before($Sender) {
-		if ($Sender->MasterView == 'admin')
-			return;
-		$BirthdayModule = new BirthdayModule();
-		$Sender->AddModule($BirthdayModule);
-	}
+    public function Base_Render_Before($Sender) {
+        if ($Sender->MasterView == 'admin') {
+            return;
+        }
+        $BirthdayModule = new BirthdayModule();
+        $Sender->AddModule($BirthdayModule);
+    }
 
-	public function OnDisable() {
-		Gdn::Set('BirthdayModule.Birthdays', null);
-	}
+    public function OnDisable() {
+        Gdn::Set('BirthdayModule.Birthdays', null);
+    }
 
 }
