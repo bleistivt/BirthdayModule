93a6601f0a30fcebd97817855e231c4ff224ac43
bleistivt <bleistivt@users.noreply.github.com>
2018-05-03 17:46:16 +0200

    remove plugininfo

--- class.birthdaymodule.plugin.php
+++ class.birthdaymodule.plugin.php
@@ -1,14 +1,5 @@
 <?php
 
-$PluginInfo['BirthdayModule'] = [
-    'Name' => 'Birthday Module',
-    'Description' => 'Adds a module showing today\'s birthdays to the Panel. Requires ProfileExtender with the "Birthday" field enabled.',
-    'Version' => '0.2',
-    'Author' => 'Bleistivt',
-    'AuthorUrl' => 'http://bleistivt.net',
-    'License' => 'GNU GPL2'
-];
-
 class BirthdayModulePlugin extends Gdn_Plugin {
 
     public function base_render_before($sender) {
